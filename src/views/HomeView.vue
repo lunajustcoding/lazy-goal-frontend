b3aa82baca7861257faf028fbaa364ad1ff7d262
luna.lu <luna.lu@dynacloud.co>
2025-01-03 16:49:38 +0800

    fix: 修正UI功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,128 @@
 <template>
   <main>
     <h1>懶人計畫</h1>
-    <button class="add-btn" @click="openAdd">+</button>
+    <button class="btn" @click="openPopup('add', 0)">+</button>
+
     <div class="listBox">
-      <div class="list">
-        <b>健身 <span>總時間:兩小時</span></b>
+      <div class="list" v-for="mainGoal in lists" :key="mainGoal.id">
+        <input type="text" v-if="mainGoal.goal == ''" />
+
+        <b v-if="mainGoal.goal !== ''"
+          >{{ mainGoal.goal }}<span>總時間:{{ mainGoal.Timing }}</span></b
+        >
+        <button class="btn" @click="openPopup('edit', mainGoal.id)">編輯</button>
         <div class="line"></div>
-        <label class="check-box">
-          <input type="checkbox" />
-          <span>把冰箱發霉的檸檬拿去丟</span>
-        </label>
+        <div class="itemBox" v-for="subGoal in mainGoal.subGoals" :key="subGoal.id">
+          <label class="check-box">
+            <input type="checkbox" />
+            <input type="text" v-if="subGoal.content == ''" />
+            <span v-else>{{ subGoal.content }}</span>
+          </label>
+          <span class="set-time">{{ subGoal.Timing }}</span>
+          <br />
+          <span v-if="subGoal.reset !== null">休息時間：{{ subGoal.reset }}</span>
+        </div>
       </div>
-      <div class="list">2</div>
+
+      <!-- <div class="list">2</div>
       <div class="list">3</div>
       <div class="list">4</div>
-      <div class="list">5</div>
+      <div class="list">5</div> -->
     </div>
   </main>
+  <!--
+  <div class="popup" v-if="editPopupRef">
+    <div class="bg">
+      <button class="close-btn" @click="editPopupRef = false">X</button>
+      <form class="from" @submit.prevent="submitGoals">
+        <div v-for="(mainGoal, mainIndex) in lists" :key="mainGoal.id" class="goal-item">
+          <input type="text" v-model="mainGoal.goal" placeholder="輸入你的主要目標" />
+          <div class="line"></div>
 
-  <div class="add-popup" v-if="addPopupRef">
-    <div class="add-bg">
-      <button class="close-btn" @click="addPopupRef = false">X</button>
-      <from class="add-from">
-        <input type="text" placeholder="輸入你的目標" />
-        <div class="line"></div>
-        <div class="itemBox" v-for="item in items" :key="item.id">
-          <input v-model="item.content" type="text" placeholder="輸入你的短期目標" />
-          <input v-model="item.reset" type="text" placeholder="輸入你的短期目標" />
+          <div class="itemBox" v-for="subGoal in mainGoal.subGoals" :key="subGoal.id">
+            <input v-model="subGoal.content" type="text" placeholder="輸入你的短期目標" />
+          </div>
+
+          <button type="button" @click="addSubGoal(mainIndex)">新增短期目標</button>
         </div>
 
-        <button class="" @click="addItem">新增項目</button>
-        <button class="" @click="addRest">新增休息</button>
-      </from>
+        <button type="submit">儲存目標</button>
+      </form>
+    </div>
+  </div> -->
+
+  <!-- 新增用 -->
+  <div class="popup" v-if="addPopupRef">
+    <div class="bg">
+      <button class="close-btn" @click="addPopupRef = false">X</button>
+      <form class="from" @submit.prevent="submitNewGoal">
+        <div class="goal-item">
+          <input type="text" placeholder="輸入你的主要目標" v-model="newGoal.goal" />
+          <label for="timing">
+            <input type="number" />
+          </label>
+          <div class="line"></div>
+          <div class="sub-goal">
+            <input type="text" />
+          </div>
+          <button class="submit">確認</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 // 彈窗
 const addPopupRef = ref(false);
+const editPopupRef = ref(false);
 
-const openAdd = () => {
-  addPopupRef.value = true;
-};
+const newGoal = reactive({
+  goal: "",
+  id: Date.now(),
+  Timing: "",
+});
 
-const items = ref([
+const lists = reactive([
   {
-    content: "",
-    reset: 10,
-    id: 1,
+    goal: "整理房間",
+    id: Date.now(),
+    Timing: "01:30:00",
+    subGoals: [
+      {
+        content: "把冰箱發霉的檸檬拿去丟",
+        Timing: "05:00",
+        reset: 10,
+        id: 1,
+      },
+    ],
   },
 ]);
-const addItem = () => {
-  items.value.push({
-    content: "",
-    id: items.value.length + 1, // 保持唯一的 id
-  });
-};
 
-const addRest = () => {
-  items.value.push({
-    reset: "",
-    content: null,
-    id: items.value.length + 1, // 保持唯一的 id
-  });
+const openPopup = (action, id) => {
+  if (action == "add" && id == 0) {
+    addPopupRef.value = true;
+    // lists.push({
+    //   goal: "",
+    //   id: Date.now(),
+    //   Timing: "",
+    //   subGoals: [
+    //     {
+    //       content: "",
+    //       Timing: "",
+    //       reset: null,
+    //       id: 1,
+    //     },
+    //   ],
+    // });
+  } else {
+    editPopupRef.value = true;
+    // const item = lists.find((i) => {
+    //   return i.id == id;
+    // });
+  }
 };
 </script>
 
@@ -78,7 +135,7 @@ main {
     font-weight: 600;
   }
 
-  .add-btn {
+  .btn {
     font-size: 1.3rem;
     cursor: pointer;
   }
@@ -105,6 +162,11 @@ main {
         span {
           font-size: 0.9rem;
         }
+      }
+
+      .set-time {
+        float: right;
+        padding-right: 23px;
       }
 
       .check-box {
@@ -159,7 +221,7 @@ main {
   }
 }
 
-.add-popup {
+.popup {
   position: absolute;
   top: 50%;
   left: 50%;
@@ -169,18 +231,29 @@ main {
   z-index: 1;
   background-color: rgba(0, 0, 0, 0.8);
 
-  .add-bg {
+  .bg {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     width: 40vw;
-    height: 70vh;
+    height: auto;
     border-radius: 25px;
+    padding: 15px 0;
     background-color: #e8e8e8;
+
+    .submit {
+      display: block;
+      border: none;
+      text-align: center;
+      font-size: 1rem;
+      background-color: transparent;
+      cursor: pointer;
+      margin: 0 auto;
+    }
   }
 
-  .add-from {
+  .from {
     display: flex;
     flex-direction: column;
     align-items: center;
